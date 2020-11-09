7b35c3517a00058d0a9e6e3cfb824c86c25edfa9
HPQuyen <48430116+HPQuyen@users.noreply.github.com>
2020-11-09 23:14:27 +0700

    actor prototype and basic login console
    - Implement actor prototype,getter/setter
    - Basic login console

---
+++ src/Actor/Admin.java
@@ -0,0 +1,11 @@
+package Actor;
+
+public class Admin extends Actor {
+
+
+    public void LoadMenuGUI() {
+        // Load Menu GUI
+        System.out.println("\t\t\tTHIS IS ADMIN MENU");
+    }
+
+}
---
+++ src/DataController/Product.java
@@ -0,0 +1,8 @@
+package DataController;
+
+public abstract class Product {
+    protected String name;
+    protected String imagePath;
+    protected String brand;
+    protected int price;
+}
